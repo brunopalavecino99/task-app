8f0ef7646134112df6810810afacffbc4c6f5684
Bruno P <brunopalavecino62@gmail.com>
2024-03-12 23:33:35 -0300

    primer commit

---
+++ task app/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,12 @@
+const TodoList = ({children}) => {
+
+    return (
+        <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl">
+            {children}
+        </div>
+
+    )
+
+}
+
+export {TodoList}
